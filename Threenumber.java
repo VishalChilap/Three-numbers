eff194f8555b378d6c0593660fd78a65040530af
VishalChilap <97787821+VishalChilap@users.noreply.github.com>
2022-01-18 19:40:09 +0530

    Add files via upload

---
+++ Threenumber.java
@@ -0,0 +1,20 @@
+package gretestnumber;
+
+public class Threenumber {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 30;
+		int num3 = 20;
+		if(num1>=num2 && num1>=num3) {
+			System.out.println(num1);		
+			}else if(num2>=num1 && num2>=num3) {
+				System.out.println(num2);
+			}
+			else {
+				System.out.println(num3);
+			}
+
+	}
+
+}
